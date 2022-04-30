c3d77a70fff2183b9a194f79f8927187520f42b4
EdwinPerezAguilar <104042704+EdwinPerezAguilar@users.noreply.github.com>
2022-04-30 17:45:24 -0500

    Add files via upload

---
+++ Terraform-ecs/variables.tf
@@ -0,0 +1,14 @@
+variable "region_az"{
+    type = string
+    default = "na-mexico-1"
+}
+
+variable "ak_huawei"{
+    type = string
+    default = "5RVHY77UWKJDOFUF9LPB"
+}
+
+variable "sk_huawei"{
+    type = string
+    default = "3Y1jq5hatcv3AFx0VZIKt045lfymEuBf2elvRsmT"
+}
